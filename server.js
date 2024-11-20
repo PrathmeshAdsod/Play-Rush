ffd27080e49e6437d55d436cccd4cc7dc1140471
Prathmesh Adsod <prathmeshadsod@gmail.com>
2024-11-20 11:38:17 +0530

    README.MD updated

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
-
 /********************              IMPORT REQUIRED PACKAGES        ******************/
 const express = require("express");
 const axios = require("axios");
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
 
-const app = express(); 
+const app = express();
 const PORT = 3000;
 
 /*********************              MIDDLWARE FOR STATIC FILES     ********************/
@@ -15,7 +13,6 @@ app.use(express.static("public"));
 // In our plays folder everything will be staatic so usign this middleware
 app.use(express.static("plays"));
 
-
 /******************************        URL ROUTING       *****************************/
 
 app.get("/", async (req, res) => {
@@ -23,12 +20,12 @@ app.get("/", async (req, res) => {
 });
 
 // Use route parameters
-app.get("/plays/:playId/:fileName",  (req, res) => {
+app.get("/plays/:playId/:fileName", (req, res) => {
   const { playId, fileName } = req.params; // get req. params in url
 
   // Construct the file path based on route parameters to send
   // file to the browser
-  const filePath = (__dirname+"/plays/"+ playId+"/" +`${fileName}`);
+  const filePath = __dirname + "/plays/" + playId + "/" + `${fileName}`;
 
   res.sendFile(filePath, (err) => {
     if (err) {
@@ -38,40 +35,15 @@ app.get("/plays/:playId/:fileName",  (req, res) => {
   });
 });
 
-// Route to serve Google Maps API via proxy
-app.get("/google-maps-api", async (req, res) => {
+app.get("/google-places-api", (req, res) => {
   const apiKey = process.env.API_KEY;
   if (!apiKey) {
-    return res.status(500).send('API key is missing');
-  } 
+    return res.status(500).send("API key is missing");
+  }
 
   try {
-    const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&v=alpha&libraries=maps3d&callback=init`;
-
-    const response = await axios.get(googleMapsUrl, {
-      responseType: 'text', 
-    });
-
-    // Set the content type to JavaScript
-    res.set('Content-Type', 'application/javascript');
-    res.send(response.data);
-   
-
-  } catch (error) {
-    console.error('Error fetching Google Maps API:', error);
-    res.status(500).send('Error fetching Google Maps API');
-  }
-});
-
-app.get('/google-places-api', (req, res) => {
-  const apiKey = process.env.API_KEY; 
-  if (!apiKey) {
-      return res.status(500).send('API key is missing');
-  }
-  
- try {
-   // Construct the script response
-   const script = `
+    // Construct the script response
+    const script = `
    (g => { 
        var h, a, k, p = "The Google Maps JavaScript API", c = "google", 
        l = "importLibrary", q = "__ib__", m = document, b = window; 
@@ -95,18 +67,13 @@ app.get('/google-places-api', (req, res) => {
    });
 `;
 
-res.set('Content-Type', 'application/javascript');
-res.send(script);
-console.log("places api loaded")
- }
- catch (error) {
-  console.error('Error fetching Google Places API:', error);
-  res.status(500).send('Error fetching Google Places API');
-}
-
-
+    res.set("Content-Type", "application/javascript");
+    res.send(script);
+  } catch (error) {
+    console.error("Error fetching Google Places API:", error);
+    res.status(500).send("Error fetching Google Places API");
+  }
 });
-
 
 /*********************************    LISTEN ON THE PORT      **************************** */
 
